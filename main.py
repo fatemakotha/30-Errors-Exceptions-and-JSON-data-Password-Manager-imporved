c7e7a2c693ee8784cdbeeefe7700d8a328536ff6
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-26 14:34:08 +0600

    Catch the exception

--- main.py
+++ main.py
@@ -1,22 +1,3 @@
 
 
 
-height = float(input("HEIGHT: "))
-weight = float(input("WEIGHT: "))
-
-#Human height is less than 3 meters
-if height > 3: #if height inputted is 45
-    raise ValueError("Human height should not be over 3 meters") #which says the the value entered is wrong and
-                                                                 #displays the message "Human height should not be over 3 meters"
-bmi = weight / height ** 2
-print(bmi)
-
-#PRINTS:
-# HEIGHT: 55
-# WEIGHT: 2
-# Traceback (most recent call last):
-#   File "C:\Users\fatem\PycharmProjects\fatemakotha\30-Errors-Exceptions-and-JSON-data-Password-Manager-imporved\main.py", line 9, in <module>
-#     raise ValueError("Human height should not be over 3 meters") #which says the the value entered is wrong and
-# ValueError: Human height should not be over 3 meters
-
-
